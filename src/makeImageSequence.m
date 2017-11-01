3187ce9c7958198d587a7cadb6ce23e619fa6466
Vectorlincent <vincent.lin@empowerops.com>
2017-10-31 18:55:21 -0700

    Another partial

--- src/makeImageSequence.m
+++ src/makeImageSequence.m
@@ -1,11 +1,15 @@
-function [handle] = makeImageSequence(workingDir, )
+function [handle] = makeImageSequence(workingDir)
 
 handle = 1;
 imageCount = 1;
 
+outputVideo = VideoWriter(fullfile(workingDir, 'output.yuv'));
 for i = 1:imageCount
     ycbcrImage = rgb2ycbcr(rgbImage);
-    imshow(ycbcrImage);
+    %imshow(ycbcrImage);
+    writeVideo(outputVideo, ycbcrImage);
 end
+
+close(outputVideo);
 
 end
