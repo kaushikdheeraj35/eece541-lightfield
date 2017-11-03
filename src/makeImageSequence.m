11089456cb1f5f88ecb88696770e7165296e066e
Vectorlincent <vincent.lin@empowerops.com>
2017-11-02 18:28:12 -0700

    Image loading stuff

--- src/makeImageSequence.m
+++ src/makeImageSequence.m
@@ -1,5 +1,26 @@
-function [handle] = makeImageSequence(workingDir)
+function [handle] = makeImageSequence(images, format)
 
+% Chroma downsampling format
+switch format
+    case '400'
+        fwidth = 0.0;
+        fheight = 0.0;
+    case '411'
+        fwidth = 0.25;
+        fheight = 1.0;
+    case '420'
+        fwidth = 0.5;
+        fheight = 0.5;
+    case '422'
+        fwidth = 0.5;
+        fheight = 1.0;
+    case '444'
+        fwidth = 1.0;
+        fheight = 1.0;
+    otherwise
+        error('Unknown chroma downsampling format.')
+end
+    
 handle = 1;
 imageCount = 1;
 
--- src/project.m
+++ src/project.m
@@ -1,1 +1,7 @@
+%% EECE 541 Light Field Project
 
+workingDir = '../resources/4_Compression/Content/Camera array/Lego Knights/rectified';
+fileNames = ls(strcat(workingDir, '/*.png'));
+for i = 1:17
+    image = imread(strtrim(fileNames(i, :)));
+end
