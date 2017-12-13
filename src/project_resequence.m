a68a6e257123c991a5fd3d402fd01f200a1b6b47
Vectorlincent <vincent.lin@empowerops.com>
2017-12-13 15:49:37 -0800

    MVC resequencing

--- src/project_resequence.m
+++ src/project_resequence.m
@@ -9,11 +9,31 @@ for gop = [4 8 16]
     end
 end
 
-% for gop = [4 8]
-%     for qp = [24 28 32 36 40]
-%         inputFiles = 
-%     end
-% end
+for gop = [4 8]
+    for qp = [24 28 32 36 40]
+        inputFiles = [
+            sprintf('../assets/mvc/recMvc420Gop%dQp%d_0.yuv ', gop, qp);
+            sprintf('../assets/mvc/recMvc420Gop%dQp%d_1.yuv ', gop, qp);
+            sprintf('../assets/mvc/recMvc420Gop%dQp%d_2.yuv ', gop, qp);
+            sprintf('../assets/mvc/recMvc420Gop%dQp%d_3.yuv ', gop, qp);
+            sprintf('../assets/mvc/recMvc420Gop%dQp%d_4.yuv ', gop, qp);
+            sprintf('../assets/mvc/recMvc420Gop%dQp%d_5.yuv ', gop, qp);
+            sprintf('../assets/mvc/recMvc420Gop%dQp%d_6.yuv ', gop, qp);
+            sprintf('../assets/mvc/recMvc420Gop%dQp%d_7.yuv ', gop, qp);
+            sprintf('../assets/mvc/recMvc420Gop%dQp%d_8.yuv ', gop, qp);
+            sprintf('../assets/mvc/recMvc420Gop%dQp%d_9.yuv ', gop, qp);
+            sprintf('../assets/mvc/recMvc420Gop%dQp%d_10.yuv', gop, qp);
+            sprintf('../assets/mvc/recMvc420Gop%dQp%d_11.yuv', gop, qp);
+            sprintf('../assets/mvc/recMvc420Gop%dQp%d_12.yuv', gop, qp);
+            sprintf('../assets/mvc/recMvc420Gop%dQp%d_13.yuv', gop, qp);
+            sprintf('../assets/mvc/recMvc420Gop%dQp%d_14.yuv', gop, qp);
+            sprintf('../assets/mvc/recMvc420Gop%dQp%d_15.yuv', gop, qp);
+            sprintf('../assets/mvc/recMvc420Gop%dQp%d_16.yuv', gop, qp);
+        ];
+        outputFile = sprintf('../assets/subjective_tests/mvc_420/recgop%dqp%d.yuv', gop, qp);
+        resequenceImages(outputFile, inputFiles, 'raster', '420', 1024, 1024);
+    end
+end
 
 % inputFiles = [
 %     '../assets/mvc/mvc_420_0.yuv ';
